ecc7970df862533b9a5a8b8662684874331c314a
Benjamin Doherty <bjd@bangpound.org>
2013-10-27 07:13:37 -0500

    use generic tag for event listener.

--- src/Rshief/PubsubBundle/EventListener/PubsubNotification.php
+++ src/Rshief/PubsubBundle/EventListener/PubsubNotification.php
@@ -12,20 +12,33 @@ use Sputnik\Bundle\PubsubBundle\PubsubEvents;
 use Sputnik\Bundle\PubsubBundle\Event\NotificationReceivedEvent;
 use Sonata\NotificationBundle\Backend\BackendInterface;
 
+/**
+ * Class PubsubNotification
+ * @package Rshief\PubsubBundle\EventListener
+ */
 class PubsubNotification implements EventSubscriberInterface {
 
     private $backend;
 
+    /**
+     * @param BackendInterface $backend
+     */
     public function __construct(BackendInterface $backend)
     {
         $this->backend = $backend;
     }
 
+    /**
+     * @return array
+     */
     public static function getSubscribedEvents()
     {
         return array(PubsubEvents::NOTIFICATION_RECEIVED => 'onNotificationReceived');
     }
 
+    /**
+     * @param NotificationReceivedEvent $event
+     */
     public function onNotificationReceived(NotificationReceivedEvent $event)
     {
         // create and publish a message
