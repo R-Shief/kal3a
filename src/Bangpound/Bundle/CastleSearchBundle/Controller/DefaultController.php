6c2e8161038f34b5d3b698eb7276ce0aabb01e78
Benjamin Doherty <bjd@bangpound.org>
2013-12-02 18:37:36 -0600

    make the search form more bootstrappy.

--- src/Bangpound/Bundle/CastleSearchBundle/Controller/DefaultController.php
+++ src/Bangpound/Bundle/CastleSearchBundle/Controller/DefaultController.php
@@ -15,13 +15,40 @@ class DefaultController extends Controller
      */
     public function searchAction()
     {
-        $form = $this->createFormBuilder()
+        $formBuilder = $this->createFormBuilder(null, array(
+            'attr' => array(
+                'class' => 'navbar-form navbar-left',
+                'role' => 'search',
+        )))
             ->setAction($this->generateUrl('bangpound_castle_search_result'))
             ->setMethod('GET')
-            ->add('queryTerm', 'text')
-            ->add('date', 'date')
-            ->add('search', 'submit')
-            ->getForm();
+            ->add('queryTerm', 'search', array(
+                'attr' => array(
+                    'placeholder' => 'Search',
+                    'class' => 'form-control',
+                ),
+            ))
+            ->add('date', 'date', array(
+                'attr' => array(
+                    'class' => 'form-group',
+                ),
+            ))
+            ->add('search', 'submit', array(
+                'attr' => array(
+                    'class' => 'btn btn-default',
+                ),
+                'label' => '<i class="glyphicon glyphicon-search"></i>',
+            ));
+
+        foreach (array('year', 'month', 'day') as $key) {
+            $child = $formBuilder->get('date')->get($key);
+            $type = $child->getType()->getName();
+            $options = $child->getOptions();
+            $options['attr'] = array('class' => 'form-control', 'style' => 'width:auto');
+            $formBuilder->get('date')->add($key, $type, $options);
+        }
+
+        $form = $formBuilder->getForm();
 
         return $this->render('BangpoundCastleSearchBundle:Default:search.html.twig', array(
             'form' => $form->createView(),
