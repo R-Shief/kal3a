73a61f33f40bfd8bb1269db815aa250494a40a03
Benjamin Doherty <bjd@bangpound.org>
2014-02-17 03:25:21 -0600

    populate trends command.

--- src/Rshief/Bundle/Kal3aBundle/Command/PopulateTrendsCommand.php
+++ src/Rshief/Bundle/Kal3aBundle/Command/PopulateTrendsCommand.php
@@ -11,14 +11,14 @@ use Symfony\Component\Console\Output\OutputInterface;
  * Class PopulateStatsCommand
  * @package Rshief\Bundle\Kal3aBundle\Command
  */
-class PopulateStatsCommand extends ContainerAwareCommand {
-
+class PopulateTrendsCommand extends ContainerAwareCommand
+{
     protected function configure()
     {
         $date = new \DateTime('now', new \DateTimeZone('UTC'));
         $date->modify('-1 hour');
 
-        $this->setName('rshief:populate:stats')
+        $this->setName('rshief:populate:trends')
             ->addArgument('design_document', InputArgument::REQUIRED, 'Design document')
             ->addArgument('view_name', InputArgument::REQUIRED, 'View name')
             ->addArgument('date', InputArgument::OPTIONAL, 'Date', $date)
@@ -28,7 +28,7 @@ class PopulateStatsCommand extends ContainerAwareCommand {
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $default_client = $this->getContainer()->get('doctrine_couchdb.client.default_connection');
-        $stats_client = $this->getContainer()->get('doctrine_couchdb.client.stats_connection');
+        $stats_client = $this->getContainer()->get('doctrine_couchdb.client.trends_connection');
 
         $design_document = $input->getArgument('design_document');
         $view_name = $input->getArgument('view_name');
@@ -46,10 +46,10 @@ class PopulateStatsCommand extends ContainerAwareCommand {
         $query->setStale('ok');
 
         $end = clone $date;
-        $end->add(new \DateInterval('P1D'));
+        $end->add(new \DateInterval('PT1H'));
 
-        $query->setStartKey([(int) $date->format('Y'), (int) $date->format('m'), (int) $date->format('d')]);
-        $query->setEndKey([(int) $end->format('Y'), (int) $end->format('m'), (int) $end->format('d')]);
+        $query->setStartKey([(int) $date->format('Y'), (int) $date->format('m'), (int) $date->format('d'), (int) $date->format('H'), (int) $date->format('i')]);
+        $query->setEndKey([(int) $end->format('Y'), (int) $end->format('m'), (int) $end->format('d'), (int) $end->format('H'), (int) $end->format('i')]);
         $query->setLimit($limit + 1);
 
         do {
@@ -58,7 +58,7 @@ class PopulateStatsCommand extends ContainerAwareCommand {
             if (count($result) > 0) {
                 $max = count($result) > $limit ? $limit : count($result);
                 $max_key = count($result) > $limit ? $limit - 1 : count($result) - 1;
-                $output->writeln(sprintf('%s -> %s', $result[0]['key'][3], $result[$max_key]['key'][3]));
+                $output->writeln(sprintf('%s -> %s', $result[0]['key'][5], $result[$max_key]['key'][5]));
 
                 if (count($result) == $limit + 1) {
                     $next_start_key = $result[$limit]['key'];
@@ -94,7 +94,9 @@ class PopulateStatsCommand extends ContainerAwareCommand {
 
                     $document['tag'] = array_pop($row['key']);
 
-                    $date = \DateTime::createFromFormat('Y-n-j|', implode('-', $row['key']));
+                    $date = new \DateTime;
+                    $date->setDate($row['key'][0], $row['key'][1], $row['key'][2]);
+                    $date->setTime($row['key'][3], $row['key'][4]);
                     $document['date'] = $date->format('Y-m-d H:i:s.u');
 
                     $document['value'] = $row['value'];
