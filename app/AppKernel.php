d1c909c53fedc7ec74777b371710f17625d2d1a7
Benjamin Doherty <bjd@bangpound.org>
2016-01-01 00:02:34 -0600

    remove invoker bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -46,7 +46,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Bangpound\Bundle\InvokerBundle\BangpoundInvokerBundle();
         }
 
         return $bundles;
