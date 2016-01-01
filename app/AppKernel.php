b19294e68338b058d583d0b63fb83af2528f56ce
Benjamin Doherty <bjd@bangpound.org>
2016-01-01 00:30:35 -0600

    remove assets bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Application\AssetsBundle\ApplicationAssetsBundle(),
             new Application\UserBundle\ApplicationUserBundle(),
             new Bangpound\Atom\DataBundle\BangpoundAtomDataBundle(),
             new Bangpound\Bundle\CastleBundle\BangpoundCastleBundle(),
