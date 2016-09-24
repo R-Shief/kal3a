bb7e68aaca4c4684dc369ca9c21860e164cac1ca
Benjamin Doherty <bjd@bangpound.org>
2016-09-24 15:31:00 -0500

    set up easy admin to manage users.

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Form\StreamParametersType;
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseController;
+
+/**
+ * Class AdminController.
+ */
+class AdminController extends BaseController
+{
+    public function createNewUserEntity()
+    {
+        return $this->get('fos_user.user_manager')->createUser();
+    }
+
+    public function prePersistUserEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+    }
+
+    public function preUpdateUserEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+    }
+}
