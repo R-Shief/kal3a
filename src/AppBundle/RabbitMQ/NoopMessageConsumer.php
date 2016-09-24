deba0465f73222bd6acddeed3a42232c3b8c802d
Benjamin Doherty <bjd@bangpound.org>
2016-09-24 01:25:18 -0500

    whitespace.

--- src/AppBundle/RabbitMQ/NoopMessageConsumer.php
+++ src/AppBundle/RabbitMQ/NoopMessageConsumer.php
@@ -7,9 +7,9 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 class NoopMessageConsumer implements ConsumerInterface
 {
-
     /**
      * @param AMQPMessage $msg The message
+     *
      * @return mixed false to reject and requeue, any other value to acknowledge
      */
     public function execute(AMQPMessage $msg)
