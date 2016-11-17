5fa05bff7e7497fd16e8ce806755e2f5c592ae5b
Benjamin Doherty <bjd@bangpound.org>
2016-11-17 13:59:28 -0600

    make elasticsearch work again.

---
+++ src/AppBundle/Consumer/ElasticsearchConsumer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Consumer;
+
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use ONGR\ElasticsearchBundle\Service\Manager;
+use PhpAmqpLib\Message\AMQPMessage;
+use Psr\Log\LoggerAwareTrait;
+use Symfony\Component\Serializer\SerializerAwareTrait;
+
+class ElasticsearchConsumer implements ConsumerInterface
+{
+    use SerializerAwareTrait;
+    use LoggerAwareTrait;
+
+    private $manager;
+    private $atomEntryClass;
+
+    public function __construct(Manager $manager, $atomEntryClass)
+    {
+        $this->manager = $manager;
+        $this->atomEntryClass = $atomEntryClass;
+    }
+
+    /**
+     * @param AMQPMessage $msg The message
+     *
+     * @return mixed false to reject and requeue, any other value to acknowledge
+     */
+    public function execute(AMQPMessage $msg)
+    {
+        $data = $this->serializer->deserialize($msg->body, $this->atomEntryClass, 'json');
+
+        $this->manager->persist($data);
+        $result = $this->manager->commit();
+
+        return ConsumerInterface::MSG_ACK;
+    }
+}
