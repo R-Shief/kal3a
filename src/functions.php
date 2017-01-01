be27bc825dfa0867aa83bc5f28563f2d1692341d
Benjamin Doherty <bjd@bangpound.org>
2016-12-31 19:21:56 -0600

    stop doing funky things to the parameters file.

--- src/functions.php
+++ src/functions.php
@@ -1,9 +1,1 @@
 <?php
-
-function app_dns_srv_parameter($hostname) {
-    $data = dns_get_record($hostname);
-    if (empty($data) || !isset($data[0]['target'])) {
-        throw new \RuntimeException($hostname .' could not be found');
-    }
-    return $data[0]['target'];
-}
