52c8341cd1a5c3da8e1131a5c6972f03cde54167
Benjamin Doherty <bjd@bangpound.org>
2017-01-11 20:09:44 -0600

    refactor EEP class into measurement class factory and service.

--- src/AppBundle/Stream/Eep.php
+++ src/AppBundle/Stream/Eep.php
@@ -18,12 +18,8 @@ use React\EventLoop\LoopInterface;
  *
  * @property LoggerInterface logger
  */
-class Eep implements Evenement\PluginInterface, EventLoop\PluginInterface, LoggerAwareInterface
+class Eep implements Evenement\PluginInterface, EventLoop\PluginInterface
 {
-    const MINUTE_MS = 60e3;
-    const SECOND_MS = 1e3;
-    const HOUR_MS = 3600e3;
-
     /**
      * @var Window\Periodic
      */
@@ -39,7 +35,6 @@ class Eep implements Evenement\PluginInterface, EventLoop\PluginInterface, Logge
      */
     private $idleTime;
 
-    use LoggerAwareTrait;
 
     public function __construct(Window\Periodic $statusCounter, Window\Periodic $statusAverager, Window\Periodic $idleTime)
     {
@@ -93,54 +88,5 @@ class Eep implements Evenement\PluginInterface, EventLoop\PluginInterface, Logge
         $this->statusCounter->tick();
         $this->statusAverager->tick();
         $this->idleTime->tick();
-    }
-
-    /**
-     * @return Window\Periodic
-     */
-    public function makeIdleTimeTracker()
-    {
-        $emitter = new Window\Periodic(new Composite([
-          new Stats\Max(), new Stats\Mean(), new Stats\Min(), new Stats\Sum(),
-        ]), self::MINUTE_MS);
-        $emitter->on('emit', function ($emit) {
-            $context = array_combine(['max', 'mean', 'min', 'total'], array_map(function ($num) {
-                return round($num, 4);
-            }, $emit));
-            $this->logger->info('idleTime', $context);
-        });
-
-        return $emitter;
-    }
-
-    /**
-     * @return Window\Periodic
-     */
-    public function makeStatusCounter()
-    {
-        $emitter = new Window\Periodic(new Stats\Count(), self::MINUTE_MS);
-        $emitter->on('emit', function ($emit) {
-            $this->logger->info(sprintf('statusCount %s in %s seconds', $emit, self::MINUTE_MS / 1000));
-        });
-
-        return $emitter;
-    }
-
-    /**
-     * @return Window\Periodic
-     */
-    public function makeStatusAverager()
-    {
-        $avg_tw = new Window\Tumbling(new Stats\Mean(), self::MINUTE_MS / self::SECOND_MS);
-        $avg_tw->on('emit', function ($emit) {
-            $this->logger->info(sprintf('statusAverage %s in %s seconds', $emit, self::SECOND_MS / 1000));
-        });
-
-        $emitter = new Window\Periodic(new Stats\Count(), self::SECOND_MS);
-        $emitter->on('emit', function ($emit) use ($avg_tw) {
-            $avg_tw->enqueue($emit);
-        });
-
-        return $emitter;
     }
 }
