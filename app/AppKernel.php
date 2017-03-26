d14d3e5174ed099b5fadb94ffe3fdfa9dd047bec
Benjamin Doherty <bjd@bangpound.org>
2017-03-26 09:19:02 -0500

    remove monitor bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,7 +22,6 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
-            new Liip\MonitorBundle\LiipMonitorBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new OldSound\RabbitMqBundle\OldSoundRabbitMqBundle(),
             new ONGR\ElasticsearchBundle\ONGRElasticsearchBundle(),
