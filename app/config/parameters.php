2b71be59e905c8f60bd92dd1792f14806d9c7c58
Benjamin Doherty <bjd@bangpound.org>
2017-10-16 20:28:17 -0500

    simplify parameters.php.

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -3,20 +3,17 @@
 declare(strict_types = 1);
 
 use DL\ConsulPhpEnvVar\Builder\ConsulEnvManagerBuilder;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-call_user_func(function (ContainerBuilder $container) {
-    $manager = (new ConsulEnvManagerBuilder())
-      ->withOverwriteEvenIfDefined(true)
-      ->build();
+$manager = (new ConsulEnvManagerBuilder())
+  ->withOverwriteEvenIfDefined(true)
+  ->build();
 
-    $mappings = [
-      'TWITTER_CONSUMER_KEY' => 'twitter/consumer_key',
-      'TWITTER_CONSUMER_SECRET' => 'twitter/consumer_secret',
-      'TWITTER_ACCESS_TOKEN' => 'twitter/1/access_token',
-      'TWITTER_ACCESS_TOKEN_SECRET' => 'twitter/1/access_token_secret',
-    ];
+$mappings = [
+  'TWITTER_CONSUMER_KEY' => 'twitter/consumer_key',
+  'TWITTER_CONSUMER_SECRET' => 'twitter/consumer_secret',
+  'TWITTER_ACCESS_TOKEN' => 'twitter/1/access_token',
+  'TWITTER_ACCESS_TOKEN_SECRET' => 'twitter/1/access_token_secret',
+];
 
-    $manager->getEnvVarsFromConsul($mappings);
-    $manager->exposeEnvironmentIntoContainer($container, $mappings);
-}, $container);
+$manager->getEnvVarsFromConsul($mappings);
+$manager->exposeEnvironmentIntoContainer($container, $mappings);
